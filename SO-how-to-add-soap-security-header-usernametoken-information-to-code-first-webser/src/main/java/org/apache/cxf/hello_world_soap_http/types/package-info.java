6161253219dcc30170c5d328e2db0f32b51fd9a3
destin <pytel.dawid@gmail.com>
2013-11-01 12:54:19 +0100

    Sample project for http://stackoverflow.com/questions/19721876/how-to-add-soap-security-header-usernametoken-information-to-code-first-webser

---
+++ SO-how-to-add-soap-security-header-usernametoken-information-to-code-first-webser/src/main/java/org/apache/cxf/hello_world_soap_http/types/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://cxf.apache.org/hello_world_soap_http/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.apache.cxf.hello_world_soap_http.types;
